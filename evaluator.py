e0c8c8d5bf4402598db9e156af4af0feda18d22e
lightnesscaster <dfj2106@columbia.edu>
2019-11-09 17:11:09 -0500

    Create evaluator.py

---
+++ evaluator.py
@@ -0,0 +1,38 @@
+import torch
+from torchvision import transforms, datasets, models
+from pil import image
+from pathlib import path
+import sys
+
+model = torch.load("wastemodel.pt")
+
+model.batch_size = 1
+
+transforms = transforms.compose([transforms.resize(224),
+                                       transforms.totensor(),])
+
+image = image.open(path(sys.argv[1]))
+
+inp = transforms(image)
+
+inp = inp.to(torch.device("cpu"))
+
+inp = inp[none]
+
+output = model(inp)
+
+prediction = int(torch.max(output.data, 1)[1].numpy())
+print(prediction)
+
+if (prediction == 0):
+    print ('cardboard')
+if (prediction == 1):
+    print ('glass')
+if (prediction == 2):
+    print ('metal')
+if (prediction == 3):
+    print ('paper')
+if (prediction == 4):
+    print('plastic')
+if (prediction ==5):
+    print('trash')
